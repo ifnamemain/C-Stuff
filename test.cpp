1b3a454f33ebad46db2213d26ca6c5e52f2b788f
Shane Allman <michael.allman@colorado.edu>
2016-09-17 15:07:04 -0600

    update

--- test.cpp
+++ test.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "myFunctions.h"
 using namespace std;
 
 int main(){
-  cout << "hello world!" << "\n";
-  return 0;
+    struct numsPtr{
+        int *first;
+        int *second;
+        short *third;
+    };
+    int x = 1, y = 2;
+    short z = 15000;
+    cout << "&x: " << &x << endl;
+    cout << "&y: " << &y << endl;
+    cout << "&z: " << &z << endl;
+    numsPtr a = {&x, &y};
+    numsPtr *b = &a;
+    char *pY = (char*)b->second-6;
+    cout << "b->second-2 = &y-2: " << b->second - 2 << endl;
+    cout << "*(short*)pY : " << *(short*)pY << endl;
+    cout << "b->second-1 = &y-1: " << b->second - 1 << endl;
+    cout << "b->second = &y: " << b->second << endl;
+    cout << "b->second+1 = &z: " << b->second + 1 << endl;
+    cout << "(b->second+1)[0] = z: " << (b->second + 1)[0] << endl;
+    
+    return 0;
 }
